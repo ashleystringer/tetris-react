6f2030c7d2177a7966a84d2f312ff7be55cf9fd2
ashleystringer <ashleylstringer@gmail.com>
2023-05-21 02:18:02 -0400

    fixed piece animation

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -80,6 +80,7 @@ export default function Board() {
         for(let r = 0; r < ROWS; r++){
             for(let c = 0; c < COLS; c++){
                 drawPixel(c, r, "white");
+                //boardArray[r][c] = "white";
             }
         }
     }
--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react'
 
-export default function Piece({ boardCtx, drawPixel, x, y, setY, isGameRunning }) { //boardCtx, boardArray
+export default function Piece({ boardCtx, drawPixel, x, isGameRunning }) { //boardCtx, boardArray
 
     //const [y, setY] = useState(0);
+    const xRef = useRef(0);
+    const yRef = useRef(0);
     const [testPiece, setTestPiece] = useState([]);
     const animRef = useRef();
-
-    useEffect(() => {
-        console.log("Y in useEffect hook");
-    }, [y]);
 
     useEffect(()=>{
         console.log('boardCtx in Piece');
@@ -26,7 +24,6 @@ export default function Piece({ boardCtx, drawPixel, x, y, setY, isGameRunning }
         console.log(`isGameRunning: ${isGameRunning}`);
         let dropTime = Date.now();
         function drop(){
-            console.log(`x: ${x}, y: ${y}`);
             let now = Date.now();
             let deltaTime = now - dropTime;
             if(deltaTime > 1000){
@@ -46,12 +43,18 @@ export default function Piece({ boardCtx, drawPixel, x, y, setY, isGameRunning }
        return () => { cancelAnimationFrame(animRef.current)}
     }, [isGameRunning]);
 
+    /*
+    const piece = () => {
+        const randomInt = Math.floor(Math.random() * pieces.length);
+        return pieces[randomInt];
+    }
+    */
 
     function drawPiece(){
         for(let r = 0; r < testPiece.length; r++){
             for(let c = 0; c < testPiece.length; c++){
                 if(testPiece[r][c]){
-                    drawPixel(r + x, c + y, "blue");   
+                    drawPixel(r + xRef.current, c + yRef.current, "blue");   
                 }
             }
         }
@@ -59,14 +62,16 @@ export default function Piece({ boardCtx, drawPixel, x, y, setY, isGameRunning }
     function undrawPiece(){
         for(let r = 0; r < testPiece.length; r++){
             for(let c = 0; c < testPiece.length; c++){
-                drawPixel(r + x, c + y, "white");
+                drawPixel(r + xRef.current, c + yRef.current, "white");
             }
         }
     }
     function pieceDown()
     {
         undrawPiece();
-        y++;
+        //y++;
+        yRef.current = yRef.current + 1;
+        console.log(yRef.current);
         //setY(num => num++);
         //console.log(setY);
         //console.log(`y: ${y}`);
@@ -74,14 +79,39 @@ export default function Piece({ boardCtx, drawPixel, x, y, setY, isGameRunning }
     }
     function pieceLeft(){
         undrawPiece();
-        x--;
+        xRef.current = xRef.current - 1;
+        //x--;
         drawPiece();
     }
     function pieceRight(){
         undrawPiece();
-        x++;
+        xRef.current = xRef.current + 1;
+        //x++;
         drawPiece();
     }
+
+    /*
+    function checkCollision(x, y){
+        for(let r = 0; r < testPiece.length; r++){
+            for(let c = 0; c < testPiece.length; c++){
+                if(!testPiece[r][c]){
+                    continue;
+                }
+
+                let offsetX = r + testPiece.x + xRef.current;
+                let offsetY = c + testPiece.y + yRef.current;
+
+                if(offsetX < 0 || offsetX >= COLS || offsetY >= ROLS){
+                    return true;
+                }
+
+                if(boardArray[r][c] !== "white"){
+                    return true;
+                }
+            }
+        }
+    }
+    */
     
     return (
         <div>
