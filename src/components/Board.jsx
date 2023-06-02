24b797a76da4a1d71781403975d74130e09aff81
ashleystringer <ashleylstringer@gmail.com>
2023-06-02 18:02:49 -0400

    .

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,107 +1,76 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import Piece from "./Piece";
 import pieceArray from "../constants/pieces";
 
 export default function Board() {
 
-    /*
-        - Create a Piece
-        - ??? Collision detection 
-        - ??? "Locking" pieces
-        - ??? Removing the bottom row of the Board
-    */
-
     const ROWS = 20;
     const COLS = 10;
     const BLOCK_SIZE = 30;
 
+    const animRef = useRef();
+    const boardContextRef = useRef();
+    const [piece, setPiece] = useState({});
+    const [boardCtx, setBoardCtx] = useState([]); 
+
     const [isGameRunning, setIsGameRunning] = useState(false);
-    const [selectedPiece, setSelectedPiece] = useState(null);
-    const [pieceOrient, setPieceOrient] = useState("");
-
-    const boardCanvasRef = useRef();
-    const [boardCtx, setBoardCtx] = useState([]);
-    const [boardArray, setBoardArray] = useState([]);
 
     useEffect(() => {
-
-        //console.log(`boardCtx: ${boardCtx}`);
-
-        //console.log(`pieceArray.length: ${pieceArray.length}`);
-
-        setBoardArray(createBoardArray());
-
+        setBoardCtx(boardContextRef.current.getContext("2d"));
         drawBoardCanvas();
-
-        //console.log(boardCanvasRef.current.getContext("2d"));
-
-        setBoardCtx(boardCanvasRef.current.getContext("2d"));
-
-        /*const newPiece = randomPiece();
-        setSelectedPiece(newPiece);
-
-        console.log(newPiece);*/
-
-    }, []);
+    });
 
     useEffect(() => {
-        console.log("addEventListener");
         document.addEventListener("keydown", handleKeyDown);
 
         return () => {
             document.removeEventListener("keydown", handleKeyDown);
-        };
-
+        }
     }, []);
 
     useEffect(() => {
-    }, [selectedPiece]);
 
-    function handleKeyDown(e){
-        //setPieceOrient(e.keyCode);
-        switch(e.keyCode){
-            case 87:
-                //rotate
-                console.log("rotate");
-                setPieceOrient("87");
-                break;
-            case 65:
-                console.log("left");
-                setPieceOrient("65");
-                //left
-                break;
-            case 68:
-                console.log("right");
-                setPieceOrient("68");
-                //right
-                break;
-            case 83:
-                console.log("down");
-                setPieceOrient("83");
-                break;
+        console.log(`pieceArray.length: ${pieceArray.length}`);
+
+        let dropTime = Date.now();
+        function drop(){
+            let now = Date.now();
+            let deltaTime = now - dropTime;
+
+            if(deltaTime > 1000){
+                dropTime = Date.now();
+                const randomInt = Math.floor(Math.random() * pieceArray.length);
+                setPiece(pieceArray[randomInt]);
+                console.log(`randomInt: ${randomInt}`);
+                console.log(piece);
+            }
+            if(isGameRunning){
+                animRef.current = requestAnimationFrame(drop);
+            }
         }
+
+        animRef.current = requestAnimationFrame(drop);
+
+        return () => { cancelAnimationFrame(animRef.current) }
+    
+    }, [isGameRunning])
+
+    function handleKeyDown(){
+        console.log("onKeyDown");
     }
 
-    function drawPixel(x, y, color){ 
-
-        const boardContext = boardCanvasRef.current.getContext("2d");
-
+    function drawPixel(x, y, color){
+        const boardContext = boardContextRef.current.getContext("2d");
+        
         boardContext.fillStyle = color;
         boardContext.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 
         boardContext.strokeStyle = "black";
-        boardContext.strokeRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);      
-        
-        /*
-        if(){
-            //if boardArray[x][y] exists
-            //alter that spot
-        }
-        */
+        boardContext.strokeRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
     }
 
     function drawBoardCanvas(){
-
         for(let r = 0; r < ROWS; r++){
             for(let c = 0; c < COLS; c++){
                 drawPixel(c, r, "white");
@@ -109,59 +78,23 @@ export default function Board() {
         }
     }
 
-    function createBoardArray(){
-        const boardArray = Array.from({length: ROWS}, () => Array(COLS).fill(0));
-        for(let r = 0; r < ROWS; r++){ //ROWS = 20
-            for(let c = 0; c < COLS; c++){ //COLS = 10
-                boardArray[c][r] = "white";
-            }
-        }
-        return boardArray;
-    }
-
-    function playGame(){
-        setIsGameRunning((prevState) => {
+    function toggleGame(){
+        setIsGameRunning(prevState => {
             return !prevState;
         });
     }
 
-    function sweep(){
-
-    }
-
-    /*
-
-    */
-    
     const randomPiece = () => {
         const randomInt = Math.floor(Math.random() * pieceArray.length);
-        return pieceArray[randomInt];
-    }
-
-    const values = {
-        boardCtx,
-        drawPixel,
-        pieceOrient,
-        setPieceOrient,
-        isGameRunning,
-        //piece: selectedPiece,
-        //setPiece: setSelectedPiece,
-        randomPiece,
-        boardArray,
-        ROWS,
-        COLS
+        //return pieceArray[randomInt];
+        return randomInt;
     };
 
     return (
         <>
-            <Piece 
-                values={values}
-            />
-            <canvas ref={boardCanvasRef} width="600" height="600"></canvas>
+            <canvas ref={boardContextRef} width={600} height={600}></canvas>
             <br/>
-            <button onClick={playGame}>
-                {isGameRunning ? "Pause Game" : "Play Game"}
-            </button>
+            <button onClick={toggleGame}>{isGameRunning ? "Pause Game" : "Play Game"}</button>
         </>
     )
 }
